912a5509c18c439c463d14ea78783e27ee8e745f
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-04-14 16:13:42 +0900

    キャラクターをAncientプロジェクトからmigrateした

---
+++ Source/Blank/Private/Characters/BlankCharacter.cpp
@@ -0,0 +1,26 @@
+﻿#include "Characters/BlankCharacter.h"
+
+ABlankCharacter::ABlankCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ABlankCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ABlankCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABlankCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/Blank/Public/Characters/BlankCharacter.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BlankCharacter.generated.h"
+
+UCLASS()
+class BLANK_API ABlankCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABlankCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+protected:
+	virtual void BeginPlay() override;
+};
