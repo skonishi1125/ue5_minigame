a84d0a2153a5d902cc69f53fc29589d141414b9d
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-04-14 16:39:36 +0900

    Pawnの派生としてBIrdクラスを用意

---
+++ Source/Blank/Private/Pawns/Bird.cpp
@@ -0,0 +1,27 @@
+﻿#include "Pawns/Bird.h"
+
+// Sets default values
+ABird::ABird()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ABird::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ABird::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/Blank/Public/Pawns/Bird.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Bird.generated.h"
+
+UCLASS()
+class BLANK_API ABird : public APawn
+{
+	GENERATED_BODY()
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+public:
+	ABird();
+
+protected:
+	virtual void BeginPlay() override;
+
+};
