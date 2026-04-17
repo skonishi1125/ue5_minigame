f81c4ee98fe31b188e1aef305f0e804e43d0b842
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-04-17 11:48:59 +0900

    BP GameMode クラス追加、キャラクターの用意

--- Source/Blank/Private/Characters/BlankCharacter.cpp
+++ Source/Blank/Private/Characters/BlankCharacter.cpp
@@ -1,14 +1,35 @@
 ﻿#include "Characters/BlankCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 ABlankCharacter::ABlankCharacter()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	//SpringArm->bUsePawnControlRotation = true; // Controler の回転を SpringArmに反映させる
+
+	bUseControllerRotationYaw = true;
+
 
 }
 
 void ABlankCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem
+			= ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(
+				PlayerController->GetLocalPlayer())
+			)
+		{
+			if (DefaultMappingContext)
+			{
+				Subsystem->AddMappingContext(DefaultMappingContext, 0);
+			}
+		}
+	}
 
 }
 
@@ -22,5 +43,50 @@ void ABlankCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (MoveAction)
+		{
+			EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABlankCharacter::Move);
+		}
+
+		if (LookAction)
+		{
+			EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ABlankCharacter::Look);
+		}
+
+	}
+
 }
 
+void ABlankCharacter::Move(const FInputActionValue& Value)
+{
+	FVector2D MoveVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		const FVector ForwardDir = GetActorForwardVector();
+		const FVector RightDir = GetActorRightVector();
+
+		float MoveSpeed = 10.f;
+
+		AddActorWorldOffset(ForwardDir * MoveVector.Y * MoveSpeed, true);
+		AddActorWorldOffset(RightDir * MoveVector.X * MoveSpeed, true);
+
+	}
+
+}
+
+void ABlankCharacter::Look(const FInputActionValue& Value)
+{
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// Controller (描写されない)に開店を加える
+		// Controller に追従するように傾く設定フラグなどがあるので、追従させたい場合は別途 true としておく
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+
+}
--- Source/Blank/Public/Characters/BlankCharacter.h
+++ Source/Blank/Public/Characters/BlankCharacter.h
@@ -2,7 +2,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "BlankCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class BLANK_API ABlankCharacter : public ACharacter
@@ -15,4 +19,19 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 protected:
 	virtual void BeginPlay() override;
+
+	// ========= 入力関連 ===========
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* LookAction;
+
+	// ========= アクション関連 ===========
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+
 };
