a5d6f3577b15f4613ce4c201e1ec2e76aa17ca6c
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-04-19 15:03:15 +0900

    Animation BluePrint でAnimationのStateに必要な値を取り出すようにした

---
+++ Source/Blank/Private/Characters/BlankAnimInstance.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Characters/BlankCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Characters/BlankAnimInstance.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UBlankAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	BlankCharacter = Cast<ABlankCharacter>(TryGetPawnOwner());
+	if (BlankCharacter != nullptr)
+	{
+		BlankCharacterMovement = BlankCharacter->GetCharacterMovement();
+	}
+
+}
+
+void UBlankAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (BlankCharacterMovement)
+	{
+		GroundSpeed = UKismetMathLibrary::VSizeXY(BlankCharacterMovement->Velocity);
+		if (GroundSpeed != 0)
+			UE_LOG(LogTemp, Log, TEXT("The vector value is: %s"), *BlankCharacterMovement->Velocity.ToString());
+	}
+
+}
---
+++ Source/Blank/Public/Characters/BlankAnimInstance.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "BlankAnimInstance.generated.h"
+
+class ABlankCharacter;
+class UCharacterMovementComponent;
+
+UCLASS()
+class BLANK_API UBlankAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	ABlankCharacter* BlankCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	UCharacterMovementComponent* BlankCharacterMovement;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	float GroundSpeed;
+
+};
