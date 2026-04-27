69a7a2af050746e84aa9ae01553232221a71b1f9
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-04-27 11:40:02 +0900

    Coin取得処理を記載するためのコンポーネントのベースを用意

---
+++ Source/Blank/Private/Components/UCoinComponent.cpp
@@ -0,0 +1,17 @@
+﻿#include "Components/UCoinComponent.h"
+
+UUCoinComponent::UUCoinComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UUCoinComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UUCoinComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
---
+++ Source/Blank/Public/Components/UCoinComponent.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "UCoinComponent.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BLANK_API UUCoinComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UUCoinComponent();
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+protected:
+	virtual void BeginPlay() override;
+};
