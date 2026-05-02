731365e1782fa6f7fa1cf5dd24880da6d8e0adc8
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-05-02 10:06:08 +0900

    Signboardクラスの用意

---
+++ Source/Blank/Private/Actors/Signboard.cpp
@@ -0,0 +1,27 @@
+﻿#include "Actors/Signboard.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+ASignboard::ASignboard()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 初期設定
+	RootScene = CreateDefaultSubobject<USceneComponent>(TEXT("RootScene"));
+	SetRootComponent(RootComponent);
+
+	SignboardMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SignboardMeshComponent"));
+	SignboardMesh->SetupAttachment(RootScene);
+
+}
+
+void ASignboard::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASignboard::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/Blank/Public/Actors/Signboard.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Signboard.generated.h"
+
+class USceneComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class BLANK_API ASignboard : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ASignboard();
+	virtual void Tick(float DeltaTime) override;
+protected:
+	virtual void BeginPlay() override;
+private:
+	// C++ からは継承不可だが、UE上からはアクセスできる
+	// RO というのは、BPでメッシュを割り当てることは可能だが、ノードなどで置き換える処理に制限をかけている
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USceneComponent> RootScene;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> SignboardMesh;
+};
