3a113bdffe24ff7a05db3b0d237883d8ca6a26ae
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-05-11 08:24:17 +0900

    materials調整

---
+++ Source/Blank/Private/Subsystem/MyGameInstanceSubsystem.cpp
@@ -0,0 +1,25 @@
+﻿#include "Subsystem/MyGameInstanceSubsystem.h"
+#include "GameFramework/GameUserSettings.h"
+
+void UMyGameInstanceSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+    Super::Initialize(Collection);
+    ApplyLowScalability();
+
+    UE_LOG(LogTemp, Warning, TEXT("UMyGameInstanceSubsystem"));
+
+}
+
+void UMyGameInstanceSubsystem::ApplyLowScalability()
+{
+    UGameUserSettings* UserSettings = GEngine->GetGameUserSettings();
+
+    if (UserSettings)
+    {
+        // 0: Low, 1: Medium, 2: High, 3: Epic, 4: Cinematic
+        UserSettings->SetOverallScalabilityLevel(0);
+
+        // 変更を適用し、設定ファイル(GameUserSettings.ini)に保存
+        UserSettings->ApplySettings(true);
+    }
+}
---
+++ Source/Blank/Private/Subsystem/MyGameInstanceSubsystem.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "MyGameInstanceSubsystem.generated.h"
+
+UCLASS()
+class UMyGameInstanceSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+    UFUNCTION(BlueprintCallable, Category = "Settings|Graphics")
+    void ApplyLowScalability();
+};
